6b9e0c9b295ae2ab5d31eecf6423ffecfec294e9
GedIra <irankundag65@gmail.com>
2024-11-10 22:46:29 +0200

    debugging 2

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from django.http import HttpResponse
 from .models import Author, Book, Library, Librarian
 from .models import Library
