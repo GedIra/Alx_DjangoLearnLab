6d10c3648a2db7fed9abaf93e63f5f37c37e727e
GedIra <irankundag65@gmail.com>
2024-11-10 22:45:13 +0200

    debugging 1

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Author, Book, Library, Librarian
+from .models import Library
 
 # Create your views here.
 
@@ -15,6 +15,10 @@ def listbooks(request):
   return render(request, 'relationship_app/list_books.html', context)
 
 class ListLibraryBooks(ListView):
+
+  books = Library.book.all()
+
+  context = {'books' : books}
   model = Library
   template_name = 'relationship_app/library_detail.html'
 
