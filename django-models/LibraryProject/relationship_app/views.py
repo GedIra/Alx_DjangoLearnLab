672e39eec942446acc1a23dfda70b0a22a9f30d0
GedIra <irankundag65@gmail.com>
2024-11-12 17:26:31 +0200

    debugging 6

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -44,7 +44,7 @@ class LibraryDetailView(ListView):
       context['library'] = get_object_or_404(Library, pk=self.kwargs.get('pk'))
       return context
   
-class RegisterView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
