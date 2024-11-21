9c401868fef07e0be65f124557513e090b0a892d
GedIra <irankundag65@gmail.com>
2024-11-21 11:41:26 +0200

    creation of custom user model2

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -51,7 +51,7 @@ class UserChangeForm(forms.ModelForm):
         fields = ["username", "password", "date_of_birth", "is_staff"]
 
 
-class UserAdmin(BaseUserAdmin):
+class  CustomUserAdmin(BaseUserAdmin):
     # The forms to add and change user instances
     form = UserChangeForm
     add_form = UserCreationForm
@@ -83,7 +83,7 @@ class UserAdmin(BaseUserAdmin):
 
 
 # Now register the new UserAdmin...
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
 # ... and, since we're not using Django's built-in permissions,
 # unregister the Group model from admin.
 admin.site.unregister(Group)
