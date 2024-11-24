5b8d8a5e5fe568bfdf3d505b9b0ec0219f871477
GedIra <irankundag65@gmail.com>
2024-11-24 23:58:23 +0200

    enforcing permissions in the views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,14 +3,19 @@ from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated 
 
 # Create your views here.
 
 class BookList(generics.ListAPIView):
+  
+  permission_classes = [IsAuthenticated]
   serializer_class = BookSerializer
   queryset = Book.objects.all()
   
 class BookViewSet(viewsets.ModelViewSet):
+  
+  permission_classes = [IsAuthenticated]
   serializer_class = BookSerializer
   queryset = Book.objects.all()
 
