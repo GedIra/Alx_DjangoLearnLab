3095e039492024d66264998e00aefdac62157ebd
GedIra <irankundag65@gmail.com>
2024-11-30 19:29:57 +0200

    advanced api project rename

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+"""_summary_
+Author table/model has only the author's name
+
+Returns:
+  _type_: _description_
+  string: the Author's name - author objects will be seen by his name in the serializers
+"""
+
+class Author(models.Model):
+  name = models.CharField(max_length=50, unique=True)
+  
+  def __str__(self) -> str:
+    return self.name
+
+"""_summary_
+Book table/model author has a foreign key to the Author table
+
+Returns:
+  _type_: _description_
+  string: Book objects will be presented as title and it's author's name. e.g: THE SUN by David F. Gorge
+
+""" 
+class Book(models.Model):
+  title = models.CharField(max_length=50)
+  author = models.ForeignKey(Author, on_delete=models.CASCADE)
+  publication_year = models.IntegerField()
+  
+  def __str__(self) -> str:
+    return f"{self.title} by {self.author}"
+
