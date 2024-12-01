f243c2a9b9ce58c509df23fc9eba5eb28a7ebd58
GedIra <irankundag65@gmail.com>
2024-12-01 11:49:43 +0200

    urls customazation

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
   path('authors/', views.AuthorsAPIView.as_view(),name='authors'),
   path('books/', views.ListView.as_view(), name='list_books'),
   path('books/<int:pk>/', views.DetailView.as_view(), name='book_detail'),
-  path('create/', views.CreateView.as_view(), name='book_create'),
-  path('update/<int:pk>/', views.UpdateView.as_view(), name='book_update'),
-  path('delete/<int:pk>/', views.DeleteView.as_view(), name='book_delete')
+  path('books/create/', views.CreateView.as_view(), name='book_create'),
+  path('books/update/<int:pk>/', views.UpdateView.as_view(), name='book_update'),
+  path('books/delete/<int:pk>/', views.DeleteView.as_view(), name='book_delete')
 
 ]
